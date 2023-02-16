77f8fab4939fa96385f306129f9030cb8d5b6f6b
Tanner Roberts <tannerbroberts@gmail.com>
2023-02-16 09:16:34 -0600

    bug fix, stops getItem from being called with undefined data in Calendar

--- src/components/ScreenStack/Calendar/Calendar.js
+++ src/components/ScreenStack/Calendar/Calendar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { cssHelper } from '../../../api/cssHelper'
-import TimeWindow from '../../../shared/Shared_TimeWindow/Shared_TimeWindow'
+// import TimeWindow from '../../../shared/Shared_TimeWindow/Shared_TimeWindow'
 
 const calendarCSS = () => {
 	const obj = {
@@ -14,7 +14,7 @@ export default function Calendar() {
 	return (
 		<div style={calendarCSS()}>
 			<h1>Calendar Placeholder</h1>
-			<TimeWindow />
+			{/* <TimeWindow /> */}
 		</div>
 	)
 }
