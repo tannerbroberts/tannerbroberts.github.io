3654e53ea8b862662de80bec12bd7609f11ee497
Tanner Roberts <tannerbroberts@gmail.com>
2024-10-09 08:50:14 -0500

    MOAR UPDATE!!!

--- src/Components/AboutTime/Component_AboutTime.test.js
+++ src/Components/AboutTime/Component_AboutTime.test.js
@@ -1,7 +1,7 @@
 import { renderHook } from "@testing-library/react";
 import { useSchedule } from "./Component_AboutTime";
 
-test('useSchedule should return an object with a "schedule" property', () => {
-  const { result } = renderHook(() => useSchedule());
+test('useSchedule should return an object with a "schedule" property', async () => {
+  const { result } = await renderHook(() => useSchedule());
   expect(result.current).toHaveProperty("addItem");
 });
