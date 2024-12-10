9bbee849f389b3653373251981aa8ac5c47fc919
Tanner Roberts <tannerbroberts@gmail.com>
2024-12-09 23:33:15 -0600

    works with lint and all

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <h1>React app in the building</h1>
   </StrictMode>,
 );
