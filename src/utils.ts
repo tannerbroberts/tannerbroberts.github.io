5c0cd5a18002aa05e1f304f57f0e017e93094f84
Tanner Roberts <tannerbroberts@gmail.com>
2024-12-11 16:25:22 -0600

    BIG refactor, trying to finally build up the scheduling model. Now I can see why nobody's taken this on before. It's a real toughy

--- src/utils.ts
+++ src/utils.ts
@@ -1,51 +1,161 @@
-/**
- * Data:
- * Events: name, length, child events, parent events
- * Schedule: scheduled events
- * Variables: quantities, states
- * 
- * operations:
- * create an event
- * delete an event
- * edit an event
-
- * schedule an event
- * schedule a recurring event
- * unschedule an event
- * unschedule a recurring event
- * get all event names
- * filter event names
- * get all events within a time range
- * 
- * contexts:
- * event
- * up next window
- * day window
- * week window
- * month window
- * accounting view?
- */
-
+type CondensedEvent = {
+  name: string;
+  length: number;
+  locations: CondensedLocation[];
+  schedule: CondensedSchedule;
+};
 export class Event {
   name: string;
   length: number;
-  schedule: Schedule;
-  parentEvents: Event[];
+  locations: Location[];
+  schedule: Schedule | null;
 
-  constructor(
-    name: string,
-    length: number,
-    schedule: Schedule = new Schedule(),
-    parentEvents: Event[] = [],
-  ) {
+  constructor(name: string, length: number, locations: Location[]) {
     this.name = name;
     this.length = length;
-    this.schedule = schedule;
-    this.parentEvents = parentEvents;
+    this.schedule = null;
+    this.locations = locations;
+  }
+
+  private getSchedule(): Schedule {
+    if (this.schedule === null) {
+      throw new Error(`Event ${this.name} has no associated schedule`);
+    }
+    return this.schedule;
+  }
+
+  public insert(event: Event, startTime: number, priorityLevel: number = 0) {
+    this.getSchedule().insert(event, startTime, priorityLevel);
+  }
+
+  public remove(event: Event) {
+    this.getSchedule().remove(event);
+  }
+
+  public moveEvent(
+    eventName: string,
+    eventStartTime: number,
+    newStartTime: number,
+  ) {
+    this.getSchedule().move(eventName, eventStartTime, newStartTime);
+  }
+
+  condense(): CondensedEvent {
+    return {
+      name: this.name,
+      length: this.length,
+      locations: this.locations.condense(),
+      schedule: this.schedule.condense(),
+    };
   }
 }
 
-export class ScheduledPrioritizedEvent {
+type CondensedSchedule = {
+  events: CondensedChild[];
+  collisions: Collision[];
+};
+type CondensedChild = {
+  name: string;
+  startTime: number;
+  priority: number;
+  busy: boolean;
+};
+class Schedule {
+  children: Child[];
+  collisions: Collision[];
+  parent: Event;
+  constructor(
+    children: Child[] = [],
+    collisions: Collision[] = [],
+    parent: Event,
+  ) {
+    this.children = children;
+    this.collisions = collisions;
+    this.parent = parent;
+  }
+
+  insert(event: Event, startTime: number, priorityLevel: number = 0) {
+    const newOne = new Child(event, startTime, priorityLevel);
+    this.collisions = [...this.collisions, ...this.getCollisions(newOne)];
+    this.children.push(newOne);
+  }
+
+  remove(event: Event) {
+    this.children = this.children.filter((ce) => ce.event !== event);
+    // remove all collisions that involve the event
+    this.collisions = this.collisions.filter(
+      (collision) =>
+        collision.event1.event !== event && collision.event2.event !== event,
+    );
+  }
+
+  move(eventName: string, eventStartTime: number, newStartTime: number) {
+    const event = this.children.find(
+      (ce) => ce.event.name === eventName && ce.startTime === eventStartTime,
+    );
+    if (event) {
+      event.startTime = newStartTime;
+      // remove all collisions and re-add them if they still exist
+      this.collisions = [
+        ...this.collisions.filter(
+          (collision) =>
+            collision.event1 !== event && collision.event2 !== event,
+        ),
+        ...this.getCollisions(event),
+      ];
+    }
+  }
+
+  getCollisions(newOne: Child): Collision[] {
+    const collisions: Collision[] = [];
+    this.children.forEach((oldOne) => {
+      const timeOverlap =
+        !oldOne.busy &&
+        newOne.startTime < oldOne.startTime + oldOne.event.length &&
+        newOne.startTime + newOne.event.length > oldOne.startTime;
+      const priorityOverlap = newOne.priority === oldOne.priority;
+      const bothBusy = newOne.busy && oldOne.busy;
+      if (timeOverlap && priorityOverlap && bothBusy) {
+        collisions.push(new Collision(oldOne, newOne));
+      }
+    });
+    return collisions;
+  }
+
+  toCondensedForm(): CondensedSchedule {
+    return {
+      children: this.children.map((ce) => ({
+        name: ce.event.name,
+        startTime: ce.startTime,
+        priority: ce.priority,
+        busy: ce.busy,
+      })),
+      collisions: this.collisions,
+    };
+  }
+
+  static fromCondensedForm(
+    condensed: CondensedSchedule,
+    parent: Event,
+  ): Schedule {
+    const store = EventStore.getList();
+    const children = condensed.children.map((ce) => {
+      const event = store.getEvent(ce.name);
+      return new Child(event, ce.startTime, ce.priority, ce.busy);
+    });
+    const collisions = condensed.collisions.map((collision) => {
+      const event1 = children.find((e) => e.event.name === collision.event1);
+      const event2 = children.find((e) => e.event.name === collision.event2);
+      if (event1 && event2) {
+        return new Collision(event1, event2);
+      }
+      throw new Error("Collision children not found");
+    });
+    return new Schedule(children, collisions, parent);
+  }
+}
+
+export class Child {
   event: Event;
   startTime: number;
   priority: number;
@@ -69,82 +179,94 @@ export class ScheduledPrioritizedEvent {
   }
 }
 
+type CondensedCollision = {
+  event1: string;
+  event2: string;
+};
 export class Collision {
-  event1: ScheduledPrioritizedEvent;
-  event2: ScheduledPrioritizedEvent;
-  constructor(
-    event1: ScheduledPrioritizedEvent,
-    event2: ScheduledPrioritizedEvent,
-  ) {
+  event1: Child;
+  event2: Child;
+  constructor(event1: Child, event2: Child) {
     this.event1 = event1;
     this.event2 = event2;
   }
+
+  toCondensedForm(): CondensedCollision {
+    return {
+      event1: this.event1.event.name,
+      event2: this.event2.event.name,
+    };
+  }
 }
 
-export class Schedule {
-  events: ScheduledPrioritizedEvent[];
-  collisions: Collision[];
-  constructor(
-    events: ScheduledPrioritizedEvent[] = [],
-    collisions: Collision[] = [],
-  ) {
-    this.events = events;
-    this.collisions = collisions;
-  }
+export class Parent {
+  event: Event;
+  startTime: number;
 
-  addEvent(event: Event, startTime: number, priorityLevel: number = 0) {
-    const newOne = new ScheduledPrioritizedEvent(
-      event,
-      startTime,
-      priorityLevel,
-    );
-    this.collisions = [...this.collisions, ...this.getCollisions(newOne)];
-    this.events.push(newOne);
+  constructor(event: Event, startTime: number) {
+    this.event = event;
+    this.startTime = startTime;
   }
+}
 
-  removeEvent(event: Event) {
-    this.events = this.events.filter((spe) => spe.event !== event);
-    // remove all collisions that involve the event
-    this.collisions = this.collisions.filter(
-      (collision) =>
-        collision.event1.event !== event && collision.event2.event !== event,
-    );
-  }
+export type EventInitInfo = {
+  name: string;
+  length: number;
+};
+export class EventStore {
+  private static instance: EventStore;
+  events: Event[];
 
-  editEventStartTime(
-    eventName: string,
-    eventStartTime: number,
-    newStartTime: number,
-  ) {
-    const event = this.events.find(
-      (spe) => spe.event.name === eventName && spe.startTime === eventStartTime,
-    );
-    if (event) {
-      event.startTime = newStartTime;
-      // remove all collisions and re-add them if they still exist
-      this.collisions = [
-        ...this.collisions.filter(
-          (collision) =>
-            collision.event1 !== event && collision.event2 !== event,
-        ),
-        ...this.getCollisions(event),
-      ];
+  private constructor() {
+    const eventList = localStorage.getItem("EventList");
+    if (eventList) {
+      const names = JSON.parse(eventList) as string[];
+      this.events = names.map((name) => {
+        const eventJSON = localStorage.getItem(`event-${name}`);
+        if (eventJSON) {
+          return Event.fromJSON(eventJSON);
+        }
+        throw new Error(`Event ${name} not found in localStorage`);
+      });
+    } else {
+      this.events = [];
     }
   }
 
-  getCollisions(newOne: ScheduledPrioritizedEvent): Collision[] {
-    const collisions: Collision[] = [];
-    this.events.forEach((oldOne) => {
-      const timeOverlap =
-        !oldOne.busy &&
-        newOne.startTime < oldOne.startTime + oldOne.event.length &&
-        newOne.startTime + newOne.event.length > oldOne.startTime;
-      const priorityOverlap = newOne.priority === oldOne.priority;
-      const bothBusy = newOne.busy && oldOne.busy;
-      if (timeOverlap && priorityOverlap && bothBusy) {
-        collisions.push(new Collision(oldOne, newOne));
-      }
-    });
-    return collisions;
+  static getList(): EventStore {
+    if (!EventStore.instance) {
+      EventStore.instance = new EventStore();
+    }
+    return EventStore.instance;
+  }
+
+  create(eventInitInfo: EventInitInfo) {
+    const { name, length } = eventInitInfo;
+    if (this.events.find((e) => e.name === name))
+      throw new Error("Event already exists in EventStore");
+    const event = new Event(name, length, [], []);
+    this.events.push(event);
+    localStorage.setItem(
+      "EventList",
+      JSON.stringify(this.events.map((e) => e.name)),
+    );
+    localStorage.setItem(`event-${event.name}`, event.toJSON());
+  }
+
+  remove(eventName: string) {
+    // Remove the event from the list of event names
+    this.events = this.events.filter((event) => event.name !== eventName);
+    localStorage.setItem("EventList", JSON.stringify(this.events));
+
+    // Remove the 'event-<name>' key from localStorage
+    localStorage.removeItem(`event-${eventName}`);
+  }
+
+  getEvent(eventName: string): Event {
+    const event = this.events.find((event) => event.name === eventName);
+    if (event) {
+      return event;
+    }
+    throw new Error(`Event ${eventName} not found in EventStore`);
   }
 }
