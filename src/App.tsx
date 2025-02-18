98f069c53574f42199bf6fa582ec41060be2e2f9
Tanner Roberts <tannerbroberts@gmail.com>
2025-02-17 20:24:19 -0600

    delete boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hire Me!</h1>
     </>
   )
 }
